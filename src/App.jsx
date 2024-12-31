3b24db88c6ccd166aeb8c0ff25311f36173c4102
aminul <rahathssn@gmail.com>
2024-12-31 13:06:26 +0600

    added basic setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-green-300">
+        <h2 className="text-3xl font-bold text-red-500">
+          This is a check program
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
