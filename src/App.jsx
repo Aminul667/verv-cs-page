c02a4bc2b97f48ca596cf5eda65c06e9c7cd5c7f
aminul <rahathssn@gmail.com>
2025-01-01 20:02:23 +0600

    added responsive nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
-// import Home from "./components/Home/Home";
 
 import Navbar from "./components/Navbar/Navbar";
 import "./index.css";
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,19 @@ import logo from "../../assets/images/logo.png";
 
 const Footer = () => {
   return (
-    <div className="bg-neutral mt-20">
-      <footer className="footer text-neutral-content items-center p-4 w-[1200px] m-auto">
-        <aside className="grid-flow-col items-center">
-          <img src={logo} alt="logo" />
-          <div className="ml-4">
+    <div className="bg-neutral mt-14 lg:mt-20">
+      <footer className="footer text-neutral-content items-center p-4 lg:w-[1200px] m-auto">
+        <aside className="grid-flow-row lg:grid-flow-col items-center">
+          <div className="w-full flex justify-between items-center">
+            <img src={logo} alt="logo" />
+            <nav className="grid-flow-col gap-4 lg:place-self-center lg:justify-self-end">
+              <div className="lg:hidden">
+                <IoIosMailUnread size={40} />
+                <p>hello@verv.org</p>
+              </div>
+            </nav>
+          </div>
+          <div className="lg:ml-4">
             <p className="leading-5 mb-2">
               The Peer-to-Peer Marketplace for Landlords and Tenants. Simple.
               Transparent. Free.
@@ -17,8 +25,8 @@ const Footer = () => {
             </p>
           </div>
         </aside>
-        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-          <div className="">
+        <nav className="hidden lg:grid-flow-col lg:gap-4 lg:place-self-center lg:justify-self-end lg:block">
+          <div>
             <IoIosMailUnread size={40} />
             <p>hello@verv.org</p>
           </div>
